fa5ea3c9a75736e6165e25a4d2bb6a1c1bfc091e
Aditya Sharma <adityaupdate21@yahoo.com>
2025-07-22 04:19:08 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>To-Do App</h1>
+      <p>Your React app is set up and working!</p>
+    </div>
+  );
+}
+
+export default App;
